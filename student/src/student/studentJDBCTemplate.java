a9963bb59e25f9e6917b52701b5ce007e8f5da50
shree <shree@shree-PC>
2017-02-18 07:39:19 -0800

    new

--- student/src/student/studentJDBCTemplate.java
+++ student/src/student/studentJDBCTemplate.java
@@ -5,22 +5,18 @@ import java.util.Properties;
 import javax.sql.DataSource;
 import java.sql.*;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 //import java.sql.Connection;
 //import java.sql.PreparedStatement;
 //import java.sql.ResultSet;
 //import java.sql.SQLException;
 import java.util.ArrayList;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import student.studentvo;
 import student.coursevo;
 import student.admissionvo;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 
 public class studentJDBCTemplate  {
