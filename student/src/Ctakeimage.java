e66220f4b2508c4fa96608f087e8f3074530129f
shree <shree@shree-PC>
2017-03-27 11:28:56 -0700

    ok

--- student/src/Ctakeimage.java
+++ student/src/Ctakeimage.java
@@ -6,10 +6,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Blob;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,9 +20,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 import javax.sql.rowset.serial.SerialBlob;
 import javax.sql.rowset.serial.SerialException;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import student.DBconnection;
 import student.studentJDBCTemplate;
 
 /**
@@ -28,9 +39,9 @@ import student.studentJDBCTemplate;
 @WebServlet("/Ctakeimage")
 public class Ctakeimage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	studentJDBCTemplate  stud=new   studentJDBCTemplate();
     ArrayList<String> sdata= new ArrayList<String>();
-
+    InputStream   image =null;	 
+    studentJDBCTemplate  stud=new   studentJDBCTemplate();
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -51,21 +62,44 @@ public class Ctakeimage extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+
+ {
+        HttpSession session = request.getSession();
+
 		// TODO Auto-generated method stub
-		
-		  HttpSession session = request.getSession();
 		if (!(request.getParameter("field")==null))
 		{
-		      String Image  = request.getParameter("field");
-		      System.out.println("studentImage"+Image.toString());
- 		      session.setAttribute("studIMG", Image);
+			  String Image  = request.getParameter("field");
+			  String studentid = (String) session.getAttribute("studentnameforid");
+			  String studentName  = (String) session.getAttribute("studentName");
+		      String Course  = (String) session.getAttribute("Course");
+		      String vaccination  =(String) session.getAttribute("vaccination");
+ 		      String blood   =(String) session.getAttribute("Blood");
+
+		     System.out.println("data"+Image);
+		    
+		        sdata.add(Image);	
+				sdata.add(studentid);	
+				sdata.add(studentName);					
+				sdata.add(Course);	
+				sdata.add(vaccination);					
+				sdata.add(blood);					
+
 		}
-	
- 			
-			getServletConfig().getServletContext().getRequestDispatcher("/takeimage.jsp").forward(request,response);
+		try {
 			
-		 
-}
+			
+			
+			stud.savefinaladmission(sdata);
+			sdata.clear();
+			getServletConfig().getServletContext().getRequestDispatcher("/takeimage.jsp").forward(request,response);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 
 	
+	}
 }
